92ab6672f5a619511b7cf41fd9c91c1742551166
DontStopPrograming <01dyamond01@gmail.com>
2023-11-27 17:24:16 -0500

    error fixed section home three images icon

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -1,21 +1,24 @@
 import './social.css'
 import CV from '../../assets/curriculum.pdf'
+import imgCisco from '/src/assets/cisco.svg'
+import imgGit from '/src/assets/github.svg'
+import imgResume from '/src/assets/file.png'
 
 export const Social = () => {
   return (
     <>
         <div className='home__social'>
-            <a href= 'https://www.credly.com/users/juan-jesus-nazario-vega/badges' className = 'home__social-icon' target = ''>
-            <img src= './src/assets/cisco.svg' alt="" />
+            <a href= 'https://www.credly.com/users/juan-jesus-nazario-vega/badges' className = 'home__social-icon' target = '__blank'>
+            <img src = {imgCisco}  alt="" />
             </a>
 
-            <a href='https://github.com/DontStopPrograming' className = 'home__social-icon' target = ''>
-            <img src= './src/assets/github.svg' alt="" />
+            <a href='https://github.com/DontStopPrograming' className = 'home__social-icon' target = '__blank'>
+            <img src = {imgGit}  alt="" />
             
             </a>
 
-            <a href= {CV} className = 'home__social-icon' target = ''>
-            <img src= './src/assets/file.png' alt="" />
+            <a href= {CV} className = 'home__social-icon' target = '__blank'>
+            <img src= {imgResume} alt="" />
             </a>
         </div>
     </>
