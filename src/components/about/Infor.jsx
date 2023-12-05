bc4f85a55c459e43b69e6a16b10b53fa2237f19c
DontStopPrograming <01dyamond01@gmail.com>
2023-12-05 12:34:01 -0500

    last update header

--- src/components/about/Infor.jsx
+++ src/components/about/Infor.jsx
@@ -5,7 +5,7 @@ export const Infor = () => {
     <div className = 'about__info grid'>
             <div className = 'about__box'>
                 <h3 className = 'about__title'> Experience </h3>
-                <span className = 'about__subtitle'> 1.5 Year</span>
+                <span className = 'about__subtitle'> 2 Year</span>
             </div>
 
             <div className = 'about__box'>
