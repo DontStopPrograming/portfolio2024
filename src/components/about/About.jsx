aa376da63f82b33e3815608597eead3f73504bb2
DontStopPrograming <01dyamond01@gmail.com>
2023-12-09 20:00:49 -0500

    corrected section about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,26 +7,24 @@ import { Infor } from './Infor'
 export const About = () => {
   return (
     <>
-    <section className = 'about section' id = 'about'>
-        <h2 className = 'section__title'> About me </h2>
-        <span className = 'section__subtitle'> My introduction </span>
+      <section className='about section' id='about'>
+        <h2 className='section__title'> About me </h2>
+        <span className='section__subtitle'> My introduction </span>
 
-        <div className= 'about__container container grid'>
-            <img src= {AboutImg} alt='' className = 'about__img' />
-            <div className = 'about__data'> 
-                <Infor />
-                <p className = 'about__description'> 
-                I live in Peru in the capital of Lima, Peru has a variety of climates and that is great.
-                My approach to technology began with video games, from there I was very interested in how video games were built based on hardware and software.
-                I graduated with Thecnical Informatic in 2021, These days, I am looking for new job opportunities in the technology field in order to add value.
-                </p>
+        <div className='about__container container grid'>
+          <img src={AboutImg} alt='' className='about__img' />
+          <div className='about__data'>
+            <Infor />
+            <p className='about__description'>
+              I'm in Lima, Peru. Fascination with video games sparked my tech journey. Obtained my degree in Informatics in 2021. Searching for thecnology opportunities to add value.
+            </p>
 
-                <a download = '' href= {CV} className = 'button button--flex'>
-                    Download CV
-                </a>
-            </div>
+            <a download='' href={CV} className='button button--flex'>
+              Download CV
+            </a>
+          </div>
         </div>
-    </section>  
+      </section>
     </>
   )
 }
