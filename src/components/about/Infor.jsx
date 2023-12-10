2a28ac9c0d71801ebca56a2992aafe41bfa45a9b
DontStopPrograming <01dyamond01@gmail.com>
2023-12-09 19:31:25 -0500

    data errors

--- src/components/about/Infor.jsx
+++ src/components/about/Infor.jsx
@@ -1,23 +1,23 @@
 import './infor.css'
 
 export const Infor = () => {
-  return (
-    <div className = 'about__info grid'>
-            <div className = 'about__box'>
-                <h3 className = 'about__title'> Experience </h3>
-                <span className = 'about__subtitle'> 2 Year</span>
+    return (
+        <div className='about__info grid'>
+            <div className='about__box'>
+                <h3 className='about__title'> Experience </h3>
+                <span className='about__subtitle'> 1 Year</span>
             </div>
 
-            <div className = 'about__box'>
-                <h3 className = 'about__title'> Completed </h3>
-                <span className = 'about__subtitle'> 5 Projects </span>
+            <div className='about__box'>
+                <h3 className='about__title'> Completed </h3>
+                <span className='about__subtitle'> 5 Projects </span>
             </div>
 
-            <div className = 'about__box'>
-                <h3 className = 'about__title'> Support </h3>
-                <span className = 'about__subtitle'> Online  </span>
+            <div className='about__box'>
+                <h3 className='about__title'> Support </h3>
+                <span className='about__subtitle'> Online  </span>
             </div>
         </div>
-    
-  )
+
+    )
 }
