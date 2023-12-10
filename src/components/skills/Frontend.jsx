991340c83aae6e8404cb350562a57a16805fccfb
DontStopPrograming <01dyamond01@gmail.com>
2023-12-09 19:45:11 -0500

    framework intl-en

--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -1,58 +1,58 @@
 import { FormattedMessage } from 'react-intl'
 
 export const Frontend = () => {
-  return (
-    <div className = 'skills__content'>
-            <h3 className = 'skills__title'> Frontend developer </h3>
+    return (
+        <div className='skills__content'>
+            <h3 className='skills__title'> Frontend developer </h3>
 
-            <div className = 'skills__box'>
-               
+            <div className='skills__box'>
+
                 {/* Framework or library */}
 
-                <div className = 'skills__group'>
-                    <div className = 'skills__data'> 
-                    <i className ='bx bx-badge-check' ></i>
+                <div className='skills__group'>
+                    <div className='skills__data'>
+                        <i className='bx bx-badge-check' ></i>
 
-                    <div>
-                        <h3 className = 'skills__name'> <FormattedMessage id = "react" /> </h3>
-                        <span className = 'skills__level'> Intermediate </span>
-                    </div>
-                    </div>
-
-                    <div className = 'skills__data'> 
-                    <i className ='bx bx-badge-check' ></i>
-
-                    <div>
-                        <h3 className = 'skills__name'> TYPESCRIPT </h3>
-                        <span className = 'skills__level'> Intermediate </span>
-                    </div>
+                        <div>
+                            <h3 className='skills__name'> <FormattedMessage id="REACT" /> </h3>
+                            <span className='skills__level'> Intermediate </span>
+                        </div>
                     </div>
 
-                    <div className = 'skills__data'> 
-                    <i className ='bx bx-badge-check' ></i>
+                    <div className='skills__data'>
+                        <i className='bx bx-badge-check' ></i>
 
-                    <div>
-                        <h3 className = 'skills__name'> MATERIAL UI </h3>
-                        <span className = 'skills__level'> Basic </span>
-                    </div>
-                    </div>
-
-                    <div className = 'skills__data'> 
-                    <i className ='bx bx-badge-check' ></i>
-
-                    <div>
-                        <h3 className = 'skills__name'> BOOSTRAP </h3>
-                        <span className = 'skills__level'> Basic </span>
-                    </div>
+                        <div>
+                            <h3 className='skills__name'> TYPESCRIPT </h3>
+                            <span className='skills__level'> Intermediate </span>
+                        </div>
                     </div>
 
-                    <div className = 'skills__data'> 
-                    <i className ='bx bx-badge-check' ></i>
+                    <div className='skills__data'>
+                        <i className='bx bx-badge-check' ></i>
 
-                    <div>
-                        <h3 className = 'skills__name'> SASS </h3>
-                        <span className = 'skills__level'> Basic </span>
+                        <div>
+                            <h3 className='skills__name'> MATERIAL UI </h3>
+                            <span className='skills__level'> Basic </span>
+                        </div>
                     </div>
+
+                    <div className='skills__data'>
+                        <i className='bx bx-badge-check' ></i>
+
+                        <div>
+                            <h3 className='skills__name'> BOOSTRAP </h3>
+                            <span className='skills__level'> Basic </span>
+                        </div>
+                    </div>
+
+                    <div className='skills__data'>
+                        <i className='bx bx-badge-check' ></i>
+
+                        <div>
+                            <h3 className='skills__name'> SASS </h3>
+                            <span className='skills__level'> Basic </span>
+                        </div>
                     </div>
 
                 </div>
