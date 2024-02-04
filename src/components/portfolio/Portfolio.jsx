a8f243227539baaca87cb22e925308b0e7d01726
DontStopPrograming <01dyamond01@gmail.com>
2024-02-03 19:20:56 -0500

    error fixed responsive 768 component Portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -9,20 +9,33 @@ export const Portfolio = () => {
     <section className='portfolio section' id='portfolio'>
       <h2 className='section__title'> Portfolio</h2>
       <span className='section__subtitle'> Recent works</span>
+
       <div className='portfolio__container container grid'>
 
 
+
         <div className='portfolio__box box__photo__first'>
-          <a className='' href='https://tendency.netlify.app'>
-            {/* <div className = 'portfolio__image'> <img src = {pruebaImg}  alt= 'react' /> </div> */}
+          <a className='photo__hover__description' href='https://tendency.netlify.app'>
+            {/* <p className='portfolio__textHover'>  </p> */}
+
+            {/* <div className='hover__content'>
+              Este es el contenido
+            </div> */}
+
             <div className='portfolio__image'>
 
             </div>
           </a>
-          <div className='portfolio__title'> <h3> E-commerce Clothes</h3> </div>
-          <div className='portfolio__description'>
+
+          <div className='portfolio__title'>
+            <h3> E-commerce Clothes</h3>
             <img src={reactImg} />
             <img src={gitImg} />
+          </div>
+
+          <div className='portfolio__description'>
+            <p> Trend, web application for the sale of fashion clothing. For now made with React.js technology </p>
+
           </div>
 
         </div>
